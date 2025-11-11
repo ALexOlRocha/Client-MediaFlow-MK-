156f8a7bdbb54c7e9dd09d658a192d8730e16041
ALexOlRocha <alexoliveirarocha2006@gmail.com>
2025-11-11 11:41:57 -0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
+    domains: [ 'localhost',
+      'server-mediaflow-mk.onrender.com',
+      'mediaflow-mk.onrender.com',],
     // aqui você adiciona o hostname
+    
   },
 };
 
